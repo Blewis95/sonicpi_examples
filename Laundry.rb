54199af0c0d79064833a7132fe9e8309c7e08a7f
Brian Lewis <blewis8958@gmail.com>
2017-05-20 22:09:34 -0400

    changed readme

--- Laundry.rb
+++ Laundry.rb
@@ -13,10 +13,29 @@ def dry_clothes(time) #drying
   sleep(time)
 end
 
-2.times do
-  wear_clothes(:bass_hard_c)
-  wash_clothes(60)
-  dry_clothes(2)
+def clothes_left_in_washer()
+  time = rand(1..10)
+  puts time
+  time
+end
+
+def clothes_clean?()
+  clean = rand(0..1)
+  puts clean
+  clean < 0.5
+end
+
+5.times do
+  if clothes_clean?()
+    wear_clothes(:bass_hard_c)
+  else
+    wash_clothes(60)
+    if clothes_left_in_washer() > 6
+      wash_clothes(80)
+    end
+    
+    dry_clothes(2)
+  end
 end
 
 wear_clothes(:ambi_piano)
